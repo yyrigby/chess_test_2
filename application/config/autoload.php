c267a1f1b878b9f0896633622891d98aea260c17
Yaeyoung Rigby <yyrigby@gmail.com>
2016-05-31 21:28:08 -0700

    session

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -79,7 +79,7 @@ $autoload['libraries'] = array('database');
 |	$autoload['drivers'] = array('cache' => 'cch');
 |
 */
-$autoload['drivers'] = array('session');
+$autoload['drivers'] = array('Session/session');
 
 /*
 | -------------------------------------------------------------------
