7dab77bdc9f7af41e2ca8bfb792dc8c13b4e6f07
Yaeyoung Rigby <yyrigby@gmail.com>
2016-05-31 20:52:03 -0700

    changed sess save path

--- application/config/config.php
+++ application/config/config.php
@@ -370,10 +370,10 @@ $config['encryption_key'] = 'anything_here';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 0;
-$config['sess_save_path'] = 'ci_sessions';
-$config['sess_match_ip'] = FALSE;
+$config['sess_save_path'] = 'application/cache/sessions';
+$config['sess_match_ip'] = TRUE;
 $config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
+$config['sess_regenerate_destroy'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
